4a0781075e7f0b29bd7901dfc716803e47f3d25e
Alexander Jeurissen <1220084+alexanderjeurissen@users.noreply.github.com>
2020-06-23 22:53:20 +0200

    Fix blockquotes, add back first post, fix mdx parsing.

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,10 +21,39 @@ module.exports = {
         name: `gatsby-starter-default`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#F7F8F9`,
+        theme_color: `#0A0A0A`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `pages`,
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `posts`,
+        path: `${__dirname}/src/posts/`,
+      },
+    },
+    {
+      resolve: "gatsby-plugin-page-creator",
+      options: {
+        path: `${__dirname}/src/posts`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-mdx`,
+      options: {
+        defaultLayouts: {
+          posts: require.resolve("./src/components/layout.js"),
+          default: require.resolve("./src/components/layout.js"),
+        },
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
